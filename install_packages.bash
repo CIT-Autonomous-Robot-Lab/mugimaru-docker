cc2f80c48026040c6c61542592f8886c1ac6ed5e
makotoyoshigoe <myoshigo0127@gmail.com>
2024-10-24 21:47:03 +0900

    Add livox

--- install_packages.bash
+++ install_packages.bash
@@ -35,13 +35,28 @@ mkdir -p $HOME/mugimaru_ws/src && cd $HOME/mugimaru_ws/src
 git clone -b mugimaru-2024/main https://github.com/nacky823/ros2_odometry_twist_converter.git
 git clone -b master https://github.com/nacky823/mugimaru_navigation2.git
 git clone -b master https://github.com/nacky823/mugimaru_launcher.git
+git clone -b humble https://github.com/nacky823/pointcloud_to_laserscan.git
 cd $HOME/mugimaru_ws
 rosdep update
 sudo apt-get update
 rosdep install -r -y --from-paths src --ignore-src --rosdistro humble
 colcon build --symlink-install
 
+## Installation Livox-SDK2
+git clone https://github.com/Livox-SDK/Livox-SDK2.git
+cd ./Livox-SDK2/
+mkdir build
+cd build
+cmake .. && make -j
+sudo make install
+
+## Installation livox_ros_driver2
+git clone -b feat/mugimaru-config https://github.com/CIT-Autonomous-Robot-Lab/livox_ros_driver2.git ws_livox/src/livox_ros_driver2
+cd ws_livox/src/livox_ros_driver2
+./build.sh humble
+
 echo "source $HOME/ros2_ws/install/setup.bash" >> $HOME/.bashrc
 echo "source $HOME/raspicat_ws/install/setup.bash" >> $HOME/.bashrc
 echo "source $HOME/mugimaru_ws/install/setup.bash" >> $HOME/.bashrc
+echo "source $HOME/ws_livox/install/setup.bash" >> $HOME/.bashrc
 echo "" >> $HOME/.bashrc
