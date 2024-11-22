dba6eda2face6c0dfe2821e48ce3a99ab431a58a
makotoyoshigoe <myoshigo0127@gmail.com>
2024-11-22 21:28:50 +0900

    Update

--- install_packages.bash
+++ install_packages.bash
@@ -10,19 +10,23 @@ git clone -b mugimaru-2024 git@github.com:CIT-Autonomous-Robot-Lab/raspicat_sim.
 git clone -b master git@github.com:CIT-Autonomous-Robot-Lab/raspimouse2.git
 git clone -b mugimaru-2024 git@github.com:CIT-Autonomous-Robot-Lab/wall_tracking.git
 git clone -b mugimaru-2024 git@github.com:CIT-Autonomous-Robot-Lab/pointcloud_to_laserscan.git
+cd ../../
 
 mkdir -p ~/mugimaru-docker/ws_livox/src
 cd ~/mugimaru-docker/ws_livox/src
 git clone -b mugimaru-2024 git@github.com:CIT-Autonomous-Robot-Lab/livox_ros_driver2.git 
+cd ../../
 
 mkdir -p ~/mugimaru-docker/nav2_ws/src
 cd ~/mugimaru-docker/nav2_ws/src
 git clone -b feat/tsukuba-challenge-2023-ex git@github.com:CIT-Autonomous-Robot-Lab/navigation2.git
 cd navigation2
 git reset b893e538ffc9667bf0a3ab0c99070439f5521718
+cd ../../
 
 mkdir -p ~/mugimaru-docker/gnss_driver_ws/src
 cd ~/mugimaru-docker/gnss_driver_ws/src
 git clone -b mugimaru-2024 git@github.com:CIT-Autonomous-Robot-Lab/nmea_navsat_driver.git
 git clone -b mugimaru-2024 git@github.com:CIT-Autonomous-Robot-Lab/ros2-ublox-zedf9p.git
+cd ../../
 cd ~/
